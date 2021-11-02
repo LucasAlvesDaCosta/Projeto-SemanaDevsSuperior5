e12a6ff7297b4a74dbfbd8294533f2672894bf1a
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-01 23:00:22 -0300

    Basic Statics Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTale from "components/DataTable";
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Olá mundo!</h1>
-    </div>
+    <>
+     <NavBar />
+     <div className="container">
+       <h1 className="text-primary"> Olá mundo!</h1>
+       <DataTale />
+     </div>
+     <Footer />
+   </>
   );
 }
 

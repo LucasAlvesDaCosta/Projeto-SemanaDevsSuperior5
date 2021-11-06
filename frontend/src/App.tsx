bcc217edf96641353f7539554a092324128cfd76
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-06 16:29:47 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
-import DataTale from "components/DataTable";
-import BarChart from "components/BarChart";
-import DonutChart from "components/DonutChart";
+import Routes from "Routes";
+
 function App() {
   return (
-    <>
-     <NavBar />
-     <div className="container">
-       <h1 className="text-primary py-3"> DashBoard - Vendas</h1>
-        <div className = "row px-3">
-          <div className = "col-sm-6">
-            <h5 className = "text-center text-secondary"> Taxa de Sucesso (%) </h5>
-             <BarChart />
-          </div>
-          <div className = "col-sm-6">
-            <h5 className = "text-center text-secondary">Todas as vendas</h5>
-             <DonutChart />
-          </div>
-        </div>
-        <div className = "py-3">
-          <h5 className = "text-primary">Todas Vendas</h5>
-
-        </div>
-       <DataTale />
-     </div>
-     <Footer />
-   </>
+    <Routes />
   );
 }
 

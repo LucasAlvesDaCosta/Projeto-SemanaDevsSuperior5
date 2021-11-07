de526c7a2484d274daa53e0540e9c3ca91c9c1bc
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-07 00:07:20 -0300

    env var 2

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -59,17 +59,6 @@ const options = {
     },
 };
 
- const mockData = {
-    labels: {
-        categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-        {
-            name: "% Sucesso",
-            data: [43.6, 67.1, 67.7, 45.6, 71.1]
-        }
-    ]
-};
 return (
     <Chart
         options={{ ...options, xaxis: chartData.labels }}
